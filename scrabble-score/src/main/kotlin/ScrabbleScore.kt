760735e4f7cf3312f8b97ff58e91b2ab91c40a58
Victor Grigoriu <grigoriu@adobe.com>
2019-10-06 19:40:24 +0300

    Scrabble score: compile

---
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -0,0 +1,5 @@
+object ScrabbleScore {
+    fun scoreWord(input: String): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
