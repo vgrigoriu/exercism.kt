8e0f28430dfb62eb66cb70e594cd3c14e473729d
Victor Grigoriu <grigoriu@adobe.com>
2019-10-06 19:54:52 +0300

    Scrabble score: implement

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,5 +1,17 @@
 object ScrabbleScore {
     fun scoreWord(input: String): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return input.map(this::scoreLetter).sum()
     }
+
+    private fun scoreLetter(ch: Char): Int =
+            when (ch.toUpperCase()) {
+                'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' -> 1
+                'D', 'G' -> 2
+                'B', 'C', 'M', 'P' -> 3
+                'F', 'H', 'V', 'W', 'Y' -> 4
+                'K' -> 5
+                'J', 'X' -> 8
+                'Q', 'Z' -> 10
+                else -> throw IllegalArgumentException("Illegal Scrabble character: $ch")
+            }
 }
