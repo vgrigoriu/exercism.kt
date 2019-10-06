256aad7205e9215909819b1bccc4f314d1448ad7
Victor Grigoriu <grigoriu@adobe.com>
2019-10-06 07:01:14 +0300

    Gigasecond: pass tests

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,8 +1,10 @@
 import java.time.LocalDate
 import java.time.LocalDateTime
+import kotlin.math.pow
 
 class Gigasecond(startTime: LocalDateTime) {
+    val secondsToAdd = 10.0.pow(9).toLong()
     constructor(startDate: LocalDate) : this(startDate.atStartOfDay())
 
-    val date: LocalDateTime = TODO()
+    val date: LocalDateTime = startTime.plusSeconds(secondsToAdd)
 }
