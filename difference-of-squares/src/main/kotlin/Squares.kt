88c4cd9cc08d3317e9bc2acd5651e8e886ee4656
Victor Grigoriu <grigoriu@adobe.com>
2019-10-08 08:45:44 +0300

    Difference of squares: compiles

---
+++ difference-of-squares/src/main/kotlin/Squares.kt
@@ -0,0 +1,13 @@
+class Squares(n: Int) {
+    fun squareOfSum(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun sumOfSquares(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun difference(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
