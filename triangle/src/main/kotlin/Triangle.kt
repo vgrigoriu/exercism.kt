1665eada1d06ce252a26a43fdba2cb54f16bd3c7
Victor Grigoriu <grigoriu@adobe.com>
2019-10-10 14:24:16 +0300

    Triangle: compile

---
+++ triangle/src/main/kotlin/Triangle.kt
@@ -0,0 +1,7 @@
+class Triangle(a: Double, b: Double, c: Double) {
+    constructor(a: Int, b: Int, c: Int): this(a.toDouble(), b.toDouble(), c.toDouble())
+
+    val isScalene: Boolean = TODO()
+    val isIsosceles: Boolean = TODO()
+    val isEquilateral: Boolean = TODO()
+}
