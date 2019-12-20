5622d2b72da733f42f2cf3407cec29d67c22c3c9
Victor Grigoriu <grigoriu@adobe.com>
2019-12-20 11:31:50 +0200

    First test

--- flatten-array/src/main/kotlin/FlattenArray.kt
+++ flatten-array/src/main/kotlin/FlattenArray.kt
@@ -1,5 +1,5 @@
 object Flattener {
     fun flatten(source: Collection<Any?>): List<Any> {
-        TODO("Implement the function to complete the task")
+        return source.filterNotNull().map { it }
     }
 }
